16c161d749243acb335abd6588a41d3c64ebf2c2
adee-121 <alwfe63@hotmail.com>
2024-12-07 20:04:48 +0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,1 +1,16 @@
+from flask import Flask
+from src.routes import register_routes
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
+
+    # تسجيل المسارات
+    register_routes(app)
+
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
 
